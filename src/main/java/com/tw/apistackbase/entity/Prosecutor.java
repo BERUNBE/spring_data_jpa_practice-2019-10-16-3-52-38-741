e54cc3ff65f13749e78f9f5477f763ef4d6fdb1d
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-17 12:01:49 +0800

    Added V5__Alter_Prosecutor_Table, added age, sex, idNumber, experience columns to Prosecutor

--- src/main/java/com/tw/apistackbase/entity/Prosecutor.java
+++ src/main/java/com/tw/apistackbase/entity/Prosecutor.java
@@ -19,6 +19,42 @@ public class Prosecutor {
     @NotNull
     private String name;
 
+    @NotNull
+    private Integer age;
+
+    @NotNull
+    private String sex;
+
+    @NotNull
+    private String idNumber;
+
+    @NotNull
+    private Integer experience;
+
+    public Integer getExperience() {
+        return experience;
+    }
+
+    public void setExperience(Integer experience) {
+        this.experience = experience;
+    }
+
+    public String getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(String idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
     public String getId() {
         return id;
     }
@@ -34,4 +70,13 @@ public class Prosecutor {
     public void setName(String name) {
         this.name = name;
     }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
 }
