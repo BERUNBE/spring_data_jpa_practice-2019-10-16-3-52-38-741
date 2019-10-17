f40678120ee6597b9034dfdcc41609119f29990d
Bernstein Berunia <bernstein.berunia@oocl.com>
2019-10-17 12:03:57 +0800

    [Bernstein&Mizuki] Modified V5 data types to match Prosecutor entity

--- src/main/resources/db/migration/V5__Alter_Prosecutor_Table.sql
+++ src/main/resources/db/migration/V5__Alter_Prosecutor_Table.sql
@@ -1,6 +1,6 @@
 alter table `PROSECUTOR` add (
-    `age` integer(3) not null,
-    `sex` varchar(10) not null,
-    `id_number` varchar(20) not null,
-    `experience` integer(3) not null
+    `age` integer(10) not null,
+    `sex` varchar(255) not null,
+    `id_number` varchar(255) not null,
+    `experience` integer(10) not null
 ) AFTER `name`;
